832de918564a9f88321f93ee5ba850bff1e8b71b
SeongMin-K <eo1tks3@naver.com>
2022-03-27 16:10:53 +0900

    Creating a Custom Data Model

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by SeongMinK on 2022/03/27.
+//  Copyright © 2022 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
