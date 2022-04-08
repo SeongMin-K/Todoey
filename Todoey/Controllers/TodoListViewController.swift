7d334dcf5aaa366ff1629228f628b350b040deff
SeongMin-K <eo1tks3@naver.com>
2022-04-08 20:44:43 +0900

    Removing Data from Realm

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -47,6 +47,7 @@ class TodoListViewController: UITableViewController {
         if let item = todoItems?[indexPath.row] {
             do {
                 try realm.write {
+//                    realm.delete(item)
                     item.done.toggle()
                 }
             } catch {
