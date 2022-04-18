924756d0bc28628e33fbc394586c5dd6ec68c619
SeongMin-K <eo1tks3@naver.com>
2022-04-18 20:03:27 +0900

    Creating Gradient Flow Cells

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import RealmSwift
+import ChameleonFramework
 
 class TodoListViewController: SwipeTableViewController {
     let realm = try! Realm()
@@ -21,6 +22,8 @@ class TodoListViewController: SwipeTableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.separatorStyle = .none
     }
 
     //MARK: - TableView Datasource Methods
@@ -34,6 +37,10 @@ class TodoListViewController: SwipeTableViewController {
         if let item = todoItems?[indexPath.row] {
             cell.textLabel?.text = item.title
             cell.accessoryType = item.done ? .checkmark : .none
+            if let color = UIColor(hexString: selectedCategory!.color)?.darken(byPercentage: CGFloat(indexPath.row) / CGFloat(todoItems!.count)) {
+                cell.textLabel?.textColor = ContrastColorOf(backgroundColor: color, returnFlat: true)
+                cell.backgroundColor = color
+            }
         } else {
             cell.textLabel?.text = "No Items Added"
         }
